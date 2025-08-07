7349b60463aa7084ba9ec81340cb05efb94effa1
Deepak <deepakworkpc@gmail.com>
2025-08-07 17:37:40 +0530

    feat: trying out bit operations

---
+++ bit.java
@@ -0,0 +1,13 @@
+class Bit {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+
+        System.out.println(a^b);
+        System.out.println(a|b);
+        System.out.println(a&b);
+        System.out.println(~a);
+        System.out.println(a<<1);
+        System.out.println(a>>1);
+    }
+}
