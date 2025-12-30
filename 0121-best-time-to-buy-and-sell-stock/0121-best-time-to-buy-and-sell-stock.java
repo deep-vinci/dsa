2d6c513c25a0fa58e9d7a9567294588edcfdbb0c
Deepak jha <deepakworkpc@gmail.com>
2025-12-31 01:25:32 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 53 ms (1.97%), Space: 110.9 MB (12.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,47 @@
+import java.util.Arrays;
+
 class Solution {
+
+    int[] prices;
+    int n;
+    int[][] dp;
+
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0;
+        this.prices = prices;
+        this.n = prices.length;
 
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else {
-                int profit = prices[i] - minPrice;
-
-                if(profit > maxProfit) maxProfit = profit;
-            }
+        dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        return maxProfit;
+        return solve(0, 0);
+    }
+
+    private int solve(int i, int holding) {
+
+        if (i == n) {
+            return 0;
+        }
+
+        if (dp[i][holding] != -1) {
+            return dp[i][holding];
+        }
+
+        int ans;
+
+        if (holding == 0) {
+            int skip = solve(i + 1, 0);
+            int buy  = -prices[i] + solve(i + 1, 1);
+            ans = Math.max(skip, buy);
+        }
+        else {
+            int skip = solve(i + 1, 1);
+            int sell = prices[i];
+            ans = Math.max(skip, sell);
+        }
+
+        dp[i][holding] = ans;
+        return ans;
     }
 }
- 
