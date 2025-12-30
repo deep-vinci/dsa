3072de33bc46314d65d7b86edc7060c44cf18cda
Deepak jha <deepakworkpc@gmail.com>
2025-12-31 00:51:43 +0530

    0121-BEST-TIME-TO-BUY-AND-SELL-STOCK Stats: Time: 1 ms (99.82%), Space: 94.3 MB (80.06%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,22 +1,5 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // brute force appraoch that gets stuck at case 200
-        
-        // int maxDiff = 0;
-        // for (int i = 0; i < prices.length; i++) {
-        //     for (int c = i + 1; c < prices.length; c++) {
-        //         if (prices[c] > prices[i]) {
-        //             int diff = prices[c] - prices[i];
-        //             // System.out.printf("%d is bigger than %d, diff = %d \n", prices[c], prices[i], diff);
-
-        //             if (diff > maxDiff) maxDiff = diff;
-        //         } 
-        //     }
-        // }
-
-        // return maxDiff;
-
-        // O(n) approach through gpt
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
