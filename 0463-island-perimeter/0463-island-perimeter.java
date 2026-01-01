4da3a3c0677351992d22843ee30ba21b4cca4fc8
Deepak jha <deepakworkpc@gmail.com>
2026-01-02 02:09:03 +0530

    0463-ISLAND-PERIMETER Stats: Time: 4 ms (99.83%), Space: 47.4 MB (25.85%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -4,29 +4,15 @@ class Solution {
         int cols = grid[0].length;
         int perimeter = 0;
 
-        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    int sides = 4;
-
-                    for (int[] d : dirs) {
-                        int ni = i + d[0];
-                        int nj = j + d[1];
-
-                        if (ni >= 0 && nj >= 0 &&
-                            ni < rows && nj < cols &&
-                            grid[ni][nj] == 1) {
-                            sides--;
-                        }
-                    }
-
-                    perimeter += sides;
+                    perimeter += 4;
+                    if (i + 1 < rows && grid[i + 1][j] == 1) perimeter -= 2;
+                    if (j + 1 < cols && grid[i][j + 1] == 1) perimeter -= 2;
                 }
             }
         }
-
         return perimeter;
     }
 }
