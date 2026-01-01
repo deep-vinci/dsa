9ceedd1e45cc9cb1ad370060ed93354198131e7a
Deepak jha <deepakworkpc@gmail.com>
2026-01-02 00:03:03 +0530

    2121-FIND-IF-PATH-EXISTS-IN-GRAPH Stats: Time: 62 ms (77.93%), Space: 250.3 MB (90.4%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -12,20 +12,40 @@ class Solution {
         return ans;
     }
 
+    // public boolean bfs(ArrayList<ArrayList<Integer>> adj, int source, int destination) {
+        
+    // }
+
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         visited = new int[n]; Arrays.fill(visited, 0);
 
-        for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
 
         for (int i = 0; i < edges.length; i++) {
             adj.get(edges[i][0]).add(edges[i][1]);
             adj.get(edges[i][1]).add(edges[i][0]);
         }    
-
         // System.out.println(adj);
-        // traverse(adj, source, destination);
-        // System.out.println(traverse(adj, source, destination));
-        return traverse(adj, source, destination);
+
+        // return traverse(adj, source, destination);
+
+        Queue<Integer> q = new LinkedList<>();
+        visited[source] = 1;
+        q.add(source);
+        while(!q.isEmpty()) {
+            int u = q.peek();
+            if (u == destination) return true;
+            q.poll();
+            // visited[u] = 1;
+            for(int x: adj.get(u)) {
+                if(visited[x] == 0) {
+                    visited[x] = 1;
+                    q.add(x);
+                }
+            }
+        }
+
+        return false;
     }
 }
