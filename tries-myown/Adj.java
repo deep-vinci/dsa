96f05d260cf18b7b4d9635ed7be75f9d13ab6440
deep-vinci <deepakworkpc@gmail.com>
2026-01-05 15:46:06 +0530

    feat: create adj list

---
+++ tries-myown/Adj.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Adj {
+    public static void main(String[] args) {
+        int[][] edges = {
+            {0, 1},
+            {0, 2},
+            {1, 2},
+            {2, 3},
+            {2, 4}
+        };
+
+        int n = 5;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            adj.get(u).add(v);
+            adj.get(v).add(u); // undirected
+        }
+
+        System.out.println(adj);
+    }
+}
