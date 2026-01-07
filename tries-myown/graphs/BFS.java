d5afa4f84c6cb9572cd68915a83caf71162fbb7a
deep-vinci <deepakworkpc@gmail.com>
2026-01-08 04:56:09 +0530

    chore: write

--- tries-myown/graphs/BFS.java
+++ tries-myown/graphs/BFS.java
@@ -1,52 +1,55 @@
-package graphs;
-import java.util.*;
+    package graphs;
+    import java.util.*;
 
-class BFS {
-    static int vis[];
-    public static void BFSTraversal(ArrayList<ArrayList<Integer>> adj, int n) {
-        Queue<Integer> queue = new LinkedList<>();
+    class BFS {
+        static int vis[];
+        public static void BFSTraversal(ArrayList<ArrayList<Integer>> adj, int n) {
+            Queue<Integer> queue = new LinkedList<>();
 
-        queue.add(n);
-        vis[n] = 1;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            System.out.println(node);
+            queue.add(n);   
+            vis[n] = 1;
 
-            for (int it : adj.get(node)) {
-                if (vis[it] == 0) {
-                    vis[it] = 1;
-                    queue.add(it);
-                }     
+            
+            while(!queue.isEmpty()) {
+                int x = queue.poll();
+
+                System.out.println(x);
+
+                for (int y : adj.get(x)) {
+                    if (vis[y] == 0) {
+                        vis[y] = 1;
+                        queue.add(y);
+                    }
+                }
             }
         }
-    }
 
-    public static void main(String[] args) {
-        int[][] edges = {
-            {0, 1},
-            {0, 2},
-            {1, 2},
-            {2, 3},
-            {2, 4}
-        };
+        public static void main(String[] args) {
+            int[][] edges = {
+                {0, 1},
+                {0, 2},
+                {1, 2},
+                {2, 3},
+                {2, 4}
+            };
 
-        int n = 5;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            int n = 5;
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+            for (int i = 0; i < n; i++) {
+                adj.add(new ArrayList<>());
+            }
+
+            for (int i = 0; i < edges.length; i++) {
+                int u = edges[i][0];
+                int v = edges[i][1];
+
+                adj.get(u).add(v);
+                adj.get(v).add(u); // undirected
+            }
+
+            // System.out.println(adj);
+            vis = new int[n];
+            BFSTraversal(adj, 0);
         }
-
-        for (int i = 0; i < edges.length; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            adj.get(u).add(v);
-            adj.get(v).add(u); // undirected
-        }
-
-        System.out.println(adj);
-        vis = new int[n];
-        BFSTraversal(adj, 0);
     }
-}
